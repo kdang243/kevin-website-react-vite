6e9b38de31aa993f0967139e13da3968520c35cf
kdang243 <kevindang243@gmail.com>
2024-06-14 00:02:47 -0400

    contacts finishing touches

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
         I'd love to hear from you, shoot me an email at kevindang243@gmail.com!
       </div>
 
-      <a className="flex  underline flex-row bg-gradient-to-r from-customblue to-gray-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out" href="mailto:kevindang243@gmail.com" rel="noopener noreferrer" target="_blank">
+      <a className="flex  underline flex-row text-white rounded-lg border-2 border-white font-bold py-2 px-4 hover:bg-leaf focus:ring transform transition hover:scale-105 duration-300 ease-in-out" href="mailto:kevindang243@gmail.com" rel="noopener noreferrer" target="_blank">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
         </svg>

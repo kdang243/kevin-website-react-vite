929f3b14f34b276411ed45ca46a2e071f7b1b813
kdang243 <kevindang243@gmail.com>
2024-06-18 00:45:42 -0400

    made buttons more noticable

--- src/components/StackTag.jsx
+++ src/components/StackTag.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 const StackTag = ({tagText}) => {
   return (
-    <span className="inline-block border-2 border-white rounded-lg px-1 py-0.5 text-white font-semibold mr-2 mb-2 lg:mb-0 decoration-clone">{tagText}</span>
+    <span className="inline-block  px-1 py-0.5 text-white font-semibold mr-2 mb-2 lg:mb-0 decoration-clone"> [{tagText}] </span>
   )
 }
 
